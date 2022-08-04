4565a03e7839580758cd74416fc6febc87c7e7d1
Jake Ashton <jakemaddenashton@gmail.com>
2022-08-03 21:13:26 -0700

    Add operate function

--- javascript.js
+++ javascript.js
@@ -14,4 +14,22 @@ const calculator = {
     divide: function(num1, num2) {
         return num1 / num2;
     },
-}
+
+    operate: function(num1, op, num2) {
+        switch (op) {
+            case '+':
+                return this.add(num1, num2);
+                break;
+            case '-':
+                return this.subtract(num1, num2);
+                break;
+            case 'x':
+                return this.multiply(num1, num2);
+                break;
+            case '÷':
+                return this.divide(num1, num2);
+                break;
+        }
+    },
+};
+
